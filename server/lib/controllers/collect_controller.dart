bc1e35a8ea24f49d611f921439ede977ff89a939
JM REMEUR <jm@remeur.fr>
2021-01-19 15:45:31 +0100

    cannot connect two times on the same isolate

--- server/lib/controllers/collect_controller.dart
+++ server/lib/controllers/collect_controller.dart
@@ -9,9 +9,12 @@ class CollectController extends ResourceController
 	@Operation.get()
 	Future<Response> collect() async
 	{
-    final socket = await WebSocketTransformer.upgrade(request.raw);
     final username = request.authorization.clientID;
+    if(collectService.exist(username)) {
+      return Response.badRequest(body: "You are already connected !");
+    }
 
+    final socket = await WebSocketTransformer.upgrade(request.raw);
     collectService.add(username, socket);
 
     return null;
