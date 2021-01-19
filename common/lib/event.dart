af265096ef28454b1d86fce2355da0973cb25b7c
JM REMEUR <jm@remeur.fr>
2021-01-19 01:22:43 +0100

    update event data

--- common/lib/event.dart
+++ common/lib/event.dart
@@ -1,25 +1,19 @@
 class Event {
   final String username;
-  bool collected;
-  int collectDate;
+  int collected;
 
   Event({
     this.username,
     this.collected,
-    this.collectDate
   });
-
-  DateTime get date => DateTime.fromMillisecondsSinceEpoch(collectDate);
 
   Map<String, dynamic> toJson() => {
     "username": username,
-    "collected": collected,
-    "collectDate": collectDate
+    "collected": collected
   };
 
   factory Event.fromJson(Map<String, dynamic> map) => Event(
     username: map["username"] as String,
-    collected: map["collected"] as bool,
-    collectDate: map["collectDate"] as int
+    collected: map["collected"] as int,
   );
 }
