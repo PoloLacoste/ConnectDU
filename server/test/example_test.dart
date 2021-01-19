968eedc809bbd7b3e12d643f2d8e8561015384f9
JM REMEUR <jm@remeur.fr>
2021-01-19 14:10:58 +0100

    some tests

--- server/test/example_test.dart
+++ server/test/example_test.dart
@@ -1,9 +1,17 @@
 import 'harness/app.dart';
 
-Future main() async {
+void main() async {
   final harness = Harness()..install();
 
-  test("GET /example returns 200 {'key': 'value'}", () async {
-    expectResponse(await harness.agent.get("/example"), 200, body: {"key": "value"});
+  test("GET / returns 404", () async {
+    expectResponse(await harness.agent.get("/"), 404);
+  });
+
+  test("POST /register returns 400", () async {
+    expectResponse(await harness.agent.post("/register"), 400);
+  });
+
+  test("POST /login returns 400", () async {
+    expectResponse(await harness.agent.post("/login"), 400);
   });
 }
